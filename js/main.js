c03227234953ebde90be469ff87b9f3c1e031cef
JALW-in <a.loskold@gmail.com>
2023-10-23 18:17:54 +0200

    hämta dom element

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-/*
-Här lägger du din JavaScript-kod
-*/
-/*test
-*/
+"use stricts";
+//hämta DOM element
+const newTodoInput = document.getElementById("newtodo"); //input textfältet för nya uppgifter
+const addButton = document.getElementById("newtodobutton"); //knapp som lägger till uppgifter
+const todoList = document.getElementById("todolist"); //listan som visar uppgifterna
+const clearButton = document.getElementById("clearbutton"); //knapp som rensar listan
+const message = document.getElementById("message"); //elementet för meddelanden
